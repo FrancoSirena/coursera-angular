a62a3e133836f00c52e8b72b06584ffd9477531d
Franco Sirena <fisrena@gmail.com>
2016-09-20 12:27:43 -0300

    New Features

--- mod1-solution/js/app.js
+++ mod1-solution/js/app.js
@@ -9,9 +9,16 @@ LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
   $scope.message = "Please enter data first";
   $scope.items = "";
+  $scope.rgb = "#FF0000";
   $scope.checkIfTooMuch = function () {
-    if ($scope.items == "")
+    if ($scope.items == ""){
+      $scope.rgb = "#FF0000";
+      $scope.message = "Please enter data first";
       return;
+    }
+
+
+    $scope.rgb = "#00FF00";
     if ($scope.items.split(",").length <= 3) {
        $scope.message = "Enjoy!";
     } else {
